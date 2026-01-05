29af94f31c741adffa0df396c096ca385b767493
toktoashik-ux <toktoashik@gmail.com>
2026-01-05 15:29:53 +0400

    Fix and customize Contact page

--- pages/contact/index.jsx
+++ pages/contact/index.jsx
@@ -1,8 +1,8 @@
 import { motion } from "framer-motion";
 import { BsArrowRight } from "react-icons/bs";
+import { useState } from "react";
 
 import { fadeIn } from "../../variants";
-import { useState } from "react";
 
 const Contact = () => {
   const [isLoading, setIsLoading] = useState(false);
@@ -11,49 +11,36 @@ const Contact = () => {
     event.preventDefault();
     setIsLoading(true);
 
-    const myForm = event.target;
-    const formData = new FormData(myForm);
-
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: new URLSearchParams(formData).toString(),
-    })
-      .then(() => alert("Thank you. I will get back to you ASAP."))
-      .catch((error) => console.log(error))
-      .finally(() => setIsLoading(false));
+    setTimeout(() => {
+      alert("Thank you. I will get back to you soon.");
+      setIsLoading(false);
+      event.target.reset();
+    }, 800);
   };
 
   return (
     <div className="h-full bg-primary/30">
-      <div className="container mx-auto py-32 text-center xl:text-left flex items-center justify-center h-full">
-        {/* text & form */}
+      <div className="container mx-auto py-32 flex items-center justify-center h-full">
         <div className="flex flex-col w-full max-w-[700px]">
-          {/* text */}
+          {/* Heading */}
           <motion.h2
             variants={fadeIn("up", 0.2)}
             initial="hidden"
             animate="show"
-            exit="hidden"
             className="h2 text-center mb-12"
           >
-            Let's <span className="text-accent">connect.</span>
+            Let&apos;s <span className="text-accent">connect</span>
           </motion.h2>
 
-          {/* form */}
+          {/* Form */}
           <motion.form
             variants={fadeIn("up", 0.4)}
             initial="hidden"
             animate="show"
-            exit="hidden"
-            className="flex-1 flex flex-col gap-6 w-full mx-auto"
+            className="flex flex-col gap-6 w-full mx-auto"
             onSubmit={handleSubmit}
             autoComplete="off"
-            autoCapitalize="off"
-            // only needed for production (in netlify) to accept form input
-            data-netlify="true"
           >
-            {/* input group */}
             <div className="flex gap-x-6 w-full">
               <input
                 type="text"
@@ -61,53 +48,47 @@ const Contact = () => {
                 placeholder="Name"
                 className="input"
                 disabled={isLoading}
-                aria-disabled={isLoading}
                 required
-                aria-required
               />
               <input
                 type="email"
                 name="email"
-                placeholder="E-mail"
+                placeholder="Email"
                 className="input"
                 disabled={isLoading}
-                aria-disabled={isLoading}
                 required
-                aria-required
               />
             </div>
+
             <input
               type="text"
               name="subject"
               placeholder="Subject"
               className="input"
               disabled={isLoading}
-              aria-disabled={isLoading}
               required
-              aria-required
             />
+
             <textarea
               name="message"
-              placeholder="Message..."
+              placeholder="Tell me about your project"
               className="textarea"
               disabled={isLoading}
-              aria-disabled={isLoading}
               required
-              aria-required
             />
+
             <button
               type="submit"
-              className="btn rounded-full border border-white/50 max-w-[170px] px-8 transition-all duration-300 flex items-center justify-center overflow-hidden hover:border-accent group"
+              className="btn rounded-full border border-white/50 max-w-[180px] px-8 transition-all duration-300 flex items-center justify-center overflow-hidden hover:border-accent group"
               disabled={isLoading}
-              aria-disabled={isLoading}
             >
               <span className="group-hover:-translate-y-[120%] group-hover:opacity-0 transition-all duration-500">
-                Let's talk
+                Let&apos;s talk
               </span>
 
               <BsArrowRight
                 className="-translate-y-[120%] opacity-0 group-hover:flex group-hover:-translate-y-0 group-hover:opacity-100 transition-all duration-300 absolute text-[22px]"
-                aria-hidden
+                aria-hidden="true"
               />
             </button>
           </motion.form>
